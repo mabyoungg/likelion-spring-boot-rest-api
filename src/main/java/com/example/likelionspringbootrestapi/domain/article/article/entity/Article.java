1a462380538667f2cc17fe97b7538790788d376a
mabyoungg <mabyoungg@gmail.com>
2023-12-19 00:10:15 +0900

    feat: 게시글 전체 조회

--- src/main/java/com/example/likelionspringbootrestapi/domain/article/article/entity/Article.java
+++ src/main/java/com/example/likelionspringbootrestapi/domain/article/article/entity/Article.java
@@ -2,6 +2,7 @@ package com.example.likelionspringbootrestapi.domain.article.article.entity;
 
 import com.example.likelionspringbootrestapi.domain.member.member.entity.Member;
 import com.example.likelionspringbootrestapi.global.jpa.BaseEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import lombok.*;
@@ -19,6 +20,7 @@ import static lombok.AccessLevel.PROTECTED;
 @ToString(callSuper = true)
 public class Article extends BaseEntity {
     @ManyToOne(fetch = LAZY)
+    @JsonIgnore
     private Member author;
     private String title;
     private String body;
