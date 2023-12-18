b713e7d77dcfdc46d208812c98697f7b8e307748
mabyoungg <mabyoungg@gmail.com>
2023-12-18 23:47:24 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/example/likelionspringbootrestapi/LikelionSpringBootRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.likelionspringbootrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionSpringBootRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikelionSpringBootRestApiApplication.class, args);
+    }
+
+}
