0251dec99b565837d506a67217dd72080b4628db
mabyoungg <mabyoungg@gmail.com>
2023-12-19 23:36:56 +0900

    test: 게시글 단건 조회 테스트 코드

--- src/main/java/com/example/likelionspringbootrestapi/domain/article/article/controller/ApiV1ArticlesController.java
+++ src/main/java/com/example/likelionspringbootrestapi/domain/article/article/controller/ApiV1ArticlesController.java
@@ -7,6 +7,7 @@ import com.example.likelionspringbootrestapi.global.rsData.RsData;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,5 +38,27 @@ public class ApiV1ArticlesController {
     @GetMapping("")
     public RsData<GetArticlesResponseBody> getArticles() {
         return RsData.of("200", "성공", new GetArticlesResponseBody(articleService.findAllByOrderByIdDesc()));
+    }
+
+    @Getter
+    public static class GetArticleResponseBody {
+        private final ArticleDto item;
+
+        public GetArticleResponseBody(Article article) {
+            item = new ArticleDto(article);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public RsData<GetArticleResponseBody> getArticle(
+            @PathVariable long id
+    ) {
+        return RsData.of(
+                "200",
+                "성공",
+                new GetArticleResponseBody(
+                        articleService.findById(id).get()
+                )
+        );
     }
 }
