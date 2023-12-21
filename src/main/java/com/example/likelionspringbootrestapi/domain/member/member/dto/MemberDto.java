1eaecb3784e16c0905a4f383e873ca5d77122bdc
mabyoungg <mabyoungg@gmail.com>
2023-12-21 23:02:25 +0900

    feat: 로그인 추가

---
+++ src/main/java/com/example/likelionspringbootrestapi/domain/member/member/dto/MemberDto.java
@@ -0,0 +1,25 @@
+package com.example.likelionspringbootrestapi.domain.member.member.dto;
+
+import com.example.likelionspringbootrestapi.domain.member.member.entity.Member;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class MemberDto {
+    private final Long id;
+    private final LocalDateTime createDate;
+    private final LocalDateTime modifyDate;
+    private final String username;
+    private final String nickname;
+    private final String apiKey;
+
+    public MemberDto(Member member) {
+        this.id = member.getId();
+        this.createDate = member.getCreateDate();
+        this.modifyDate = member.getModifyDate();
+        this.username = member.getUsername();
+        this.nickname = member.getNickname();
+        this.apiKey = member.getApiKey();
+    }
+}
